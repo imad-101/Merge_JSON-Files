80e82df47965a3622f1e974e39b82f1d627962b0
imad-101 <imaduddin10.1973.com>
2025-02-04 15:17:27 +0500

    Added vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -51,6 +51,7 @@ export default function RootLayout({
       <body className={`${myFont.className}  antialiased bg-yellow-50`}>
         <Header />
         {children}
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
