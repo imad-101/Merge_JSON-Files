fba27cbee50d78d099469f7c760fe962b675341b
imad-101 <imaduddin10.1973.com>
2025-02-12 19:32:08 +0500

    favicon fixes

--- app/(Json-Flattener)/json-flattener/layout.tsx
+++ app/(Json-Flattener)/json-flattener/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#fefce8" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.png" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <script
