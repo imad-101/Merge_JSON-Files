687cb88ba3a2d950a8a5d42d6305a263626c4d97
imad-101 <imaduddin10.1973.com>
2025-04-03 00:51:55 +0500

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,12 @@
-import { getSortedPostsData } from "@/lib/posts";
+const sitemap = [
+  {
+    url: "https://www.merge-json-files.com",
+    lastModified: new Date(),
+  },
+  {
+    url: "https://www.merge-json-files.com/json-file-splitter",
+    lastModified: new Date(),
+  },
+];
 
-export default async function sitemap() {
-  const posts = getSortedPostsData();
-  const postUrls = posts.map((post) => ({
-    url: `https://www.merge-json-files.com/blog/${post.id}`,
-    lastModified: new Date(post.date),
-  }));
-
-  return [
-    {
-      url: "https://www.merge-json-files.com",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.merge-json-files.com/json-file-splitter",
-      lastModified: new Date(),
-    },
-    ...postUrls,
-  ];
-}
+export default sitemap;
