dfe204b6137819bb4c5ad0f2879bb318eb65f467
imad-101 <imaduddin10.1973.com>
2025-04-03 00:57:10 +0500

    fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,12 @@
-const sitemap = [
-  {
-    url: "https://www.merge-json-files.com",
-    lastModified: new Date(),
-  },
-  {
-    url: "https://www.merge-json-files.com/json-file-splitter",
-    lastModified: new Date(),
-  },
-];
-
-export default sitemap;
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.merge-json-files.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.merge-json-files.com/json-file-splitter",
+      lastModified: new Date(),
+    },
+  ];
+}
